9566b6e69b9b4af17f2311c1711022e030b45512
David Lescano <davidlescanoar@gmail.com>
2025-05-27 20:32:03 -0300

    Created RatingHelper

---
+++ MoviesApi/Helpers/RatingHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesApi.Helpers
+{
+    public static class RatingHelper
+    {
+        public static double RoundToNearest(double value, double step)
+        {
+            return Math.Round(value / step, MidpointRounding.AwayFromZero) * step;
+        }
+    }
+}
