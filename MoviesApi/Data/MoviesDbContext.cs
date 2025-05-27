6f1902574a6c6dfebae3011a230acadc7f39f130
David Lescano <davidlescanoar@gmail.com>
2025-05-27 19:49:18 -0300

    Created DbContext

---
+++ MoviesApi/Data/MoviesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.Models;
+
+namespace MoviesApi.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies => Set<Movie>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Rating> Ratings => Set<Rating>();
+        public DbSet<Genre> Genres => Set<Genre>();
+    }
+}
